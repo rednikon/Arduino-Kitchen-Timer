d5f4955eedc2db08246e051278eb2388f014eaf2
Vee McClure <30158376+rednikon@users.noreply.github.com>
2019-10-30 22:47:06 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Kitchen Timer Project
+//
+//  Created by Vee McClure on 10/30/19.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
